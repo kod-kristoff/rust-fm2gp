2007bf8376b3f2f1a9a53df6be4477462c97f955
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-05-24 10:39:59 +0200

    add power_accumulate

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 #[inline]
 pub fn half(n: i32) -> i32 {
     n >> 1
@@ -11,28 +10,61 @@ pub fn is_odd(n: i32) -> bool {
 
 pub fn power_recursive(x: i32, n: i32) -> i32 {
 	if n == 0 {
-		1	
+		1
 	} else if n == 1 {
 		x
 	} else {
-		if n % 2 == 1 {
+		if is_odd(n) {
 			x * power_recursive(x*x, n / 2)
 		} else {
 			power_recursive(x*x, n/2)
 		}
 	}
-	
+
+}
+
+pub fn power_accumulate(r: i32, x: i32, n: i32) -> i32 {
+	if n == 0 {
+		return r
+	}
+	let mut r = r;
+	let mut x = x;
+	let mut n = n;
+	loop {
+		if is_odd(n) {
+			r = r * x;
+			if n == 1 {
+				return r
+			}
+		}
+		n = half(n);
+		x = x * x;
+	}
 }
 
 #[cfg(test)]
 mod tests {
+	use power_accumulate;
 	use power_recursive;
-    use half; 
+    use half;
     use is_odd;
-	
+
 	#[test]
 	fn power_recursive_i32_n0() {
 		assert_eq!(power_recursive(2i32, 0), 1);
+		assert_eq!(power_recursive(2, 1), 2);
+		assert_eq!(power_recursive(2, 2), 4);
+		assert_eq!(power_recursive(2, 3), 8);
+		assert_eq!(power_recursive(2, 4), 16);
+	}
+
+    #[test]
+	fn power_accumulate_i32_n0() {
+		assert_eq!(power_accumulate(1, 2i32, 0), 1);
+		assert_eq!(power_accumulate(1, 2, 1), 2);
+		assert_eq!(power_accumulate(1, 2, 2), 4);
+		assert_eq!(power_accumulate(1, 2, 3), 8);
+		assert_eq!(power_accumulate(1, 2, 4), 16);
 	}
 
     #[test]
