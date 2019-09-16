76b3e375f0871bf038decc8aa324b92b0ba30aea
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-09-16 09:59:44 +0200

    add comma

--- src/ops.rs
+++ src/ops.rs
@@ -4,14 +4,14 @@ use std::ops::Mul;
 
 pub fn mul<T>(x: T, y: T) -> T
 where
-    T: Copy + Mul<Output = T>
+    T: Copy + Mul<Output = T>,
 {
     x * y
 }
 
 pub fn add<T>(x: T, y: T) -> T
 where
-    T: Copy + Add<Output = T>
+    T: Copy + Add<Output = T>,
 {
     x + y
 }
